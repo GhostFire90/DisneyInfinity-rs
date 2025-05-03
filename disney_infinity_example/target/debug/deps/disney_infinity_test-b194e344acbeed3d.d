f873929375358f16e663a444a997dd70085f9675
Persephone <evan.anderson_@outlook.com>
2025-05-02 23:17:48 -0700

    forgot to ignore

--- disney_infinity_example/target/debug/deps/disney_infinity_test-b194e344acbeed3d.d
+++
@@ -1,5 +0,0 @@
-/home/rei/git-repos/di-usb-port/disney_infinity_test/target/debug/deps/libdisney_infinity_test-b194e344acbeed3d.rmeta: src/main.rs
-
-/home/rei/git-repos/di-usb-port/disney_infinity_test/target/debug/deps/disney_infinity_test-b194e344acbeed3d.d: src/main.rs
-
-src/main.rs:
--- disney_infinity_lib/target/debug/deps/disney_infinity_lib-3214271bceedbfa6.d
+++
@@ -1,5 +0,0 @@
-/home/rei/git-repos/di-usb-port/disney_infinity_lib/target/debug/deps/libdisney_infinity_lib-3214271bceedbfa6.rmeta: src/lib.rs
-
-/home/rei/git-repos/di-usb-port/disney_infinity_lib/target/debug/deps/disney_infinity_lib-3214271bceedbfa6.d: src/lib.rs
-
-src/lib.rs:
